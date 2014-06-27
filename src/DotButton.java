362aefe1b5b267a9cd56fd76192863a00b6130f9
EPTG <EiM_GTPE_@live.com>
2014-06-27 15:05:52 +0900

    DotButtonのJavaDocを書いた

--- src/DotButton.java
+++ src/DotButton.java
@@ -2,19 +2,19 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
-/** 数字ボタンの生成と数字ボタンが押された際の処理を行う */
+/** ドットボタンの表示とドットボタンが押された際の処理を行う */
 public class DotButton extends JButton implements MouseListener{
 
     /** 表示する数 */
     private DisplayedCalculationFormula dispNumber;
 
-    /** 引数で与えられた数字でボタンを生成し、フィールドの表示する数に引数の表示する数を代入 */
+    /** フィールドの表示する数に引数の表示する数を代入、ボタンの状態の監視を始める。 */
     public DotButton(DisplayedCalculationFormula dispNumber){
     	super(".");
     	this.dispNumber=dispNumber;
     	addMouseListener(this);
     }
-    /** 数字ボタンが押された */
+    /** ドットボタンが押された */
     public void mouseClicked(MouseEvent e){
 	dispNumber.setDisplayedCalculationFormula(".");
 	dispNumber.setSlashCalculationFormula(".");
