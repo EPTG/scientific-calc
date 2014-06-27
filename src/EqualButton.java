b61edb83e3b563ffd3d6000809d56dd0e88af9da
EPTG <EiM_GTPE_@live.com>
2014-06-27 15:25:38 +0900

    EqualButtonのJavaDocを書いた。

--- src/EqualButton.java
+++ src/EqualButton.java
@@ -7,15 +7,18 @@ public class EqualButton extends JButton implements MouseListener{
 
 	/** 演算器 */
     private ArithmeticUnit arithmUnit;
+    
+    /** 表示する数式 */
     private DisplayedCalculationFormula dispCalc;
-    /** =ボタンを生成し、フィールドの演算器に引数の演算器を代入 */
+    
+    /** =ボタンを生成し、フィールドの演算器と表示する数式に引数の演算器を代入 */
     public EqualButton(ArithmeticUnit arithmUnit,DisplayedCalculationFormula dispCalc){
     	super("=");
     	this.arithmUnit = arithmUnit;
     	this.dispCalc=dispCalc;
     	addMouseListener(this);
     }
-    /** ＝ボタンが押された */
+    /** ＝ボタンが押された。スラッシュ付きの数式を読み出し、文字列をスラッシュごとに切り分けて配列の要素とする。その配列をArrayListに変換し、演算を行う*/
     public void mouseClicked(MouseEvent e){
     	try{
     	dispCalc.setNewDisplayedCalculationFormula(arithmUnit.operate(arithmUnit.convert(arithmUnit.split_op_num())));
