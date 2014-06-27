12f765e820f18d96d63aaebb3a4085892f9ed6d1
EPTG <EiM_GTPE_@live.com>
2014-06-27 15:03:27 +0900

    DisplayedCalculationFormulaのJavaDocを書いた

--- src/DisplayedCalculationFormula.java
+++ src/DisplayedCalculationFormula.java
@@ -8,12 +8,14 @@ public class DisplayedCalculationFormula extends JLabel{
 
     /** 現在の式 */
     private String disp;
+    /** 現在の式の/つき */
     private String slash;
+    /** 演算完了フラグ */
     private Boolean finishedFlag;
+    /** 演算完了時に演算結果を消すボタンを列挙した配列 */
     private String[] Clearstr = {"0","1","2","3","4","5","6","7","8","9","Ans","(",")","√(","e","log(","ln(","Sin(","Cos(","Tan(","rad(","π"};
 
-
-    /** 表示する式の初期化をし初期表示を行う*/
+    /** 表示する数式の初期化をし初期表示を行う*/
     public DisplayedCalculationFormula(){
     	disp="";
     	slash="";
@@ -21,7 +23,7 @@ public class DisplayedCalculationFormula extends JLabel{
     	super.setText(disp);//初期表示を変更した(8回より前ではsuper.setTextだったがこちらの方に変更した)
     }
 
-    /** 表示する式を空欄にする */
+    /** 表示する数式を空欄にする */
     public void clear(){
     	disp= "";
     	slash="";
@@ -29,7 +31,7 @@ public class DisplayedCalculationFormula extends JLabel{
     	super.setText(disp);
     }
 
-    /** 表示している式に引数を加える */
+    /** 表示している数式に引数を加える */
     public void setDisplayedCalculationFormula(String o){
 	if(checkFlag()){
 		unsetFlag();
@@ -45,20 +47,21 @@ public class DisplayedCalculationFormula extends JLabel{
     	slash=slash+o;
     }
 
-    /** 表示する式を読み出す */
+    /** 表示する数式を読み出す */
     public String getDisplayedCalculationFormula(){
     	return disp;
     }
-    /** 表示する式に代入*/
+    /** 表示する数式に代入*/
     public void setNewDisplayedCalculationFormula(String s){
     	disp=s;
     	slash=s;
     	super.setText(disp);
     }
-    /** 演算子の前後にスラッシュをつけた文を読み出す */
+    /** 表示する数式の演算子の前後にスラッシュをつけた文を読み出す */
     public String getslash(){
     	return slash;
     }
+    /** 表示する数式の最後の文字を削除する。またスラッシュ付きの文は表示する数式に合わせる。*/
     public void Delate(){
     	if(disp.length()==0){
     		return;
@@ -92,16 +95,20 @@ public class DisplayedCalculationFormula extends JLabel{
     	System.out.println(slash);
     	super.setText(disp);
     }
+    
+    /** 演算完了フラグを立てる */
     public void setFlag(){
 	finishedFlag=true;
     }
+    /** 演算完了フラグを解除する */
     public void unsetFlag(){
 	finishedFlag=false;
     }
+    /** 演算完了フラグを確認する。*/
     public boolean checkFlag(){
 	return finishedFlag;
     }
-    /** Errorを表示する　*/
+    /** Errorを表示する*/
     public void setError(){//Errorを表示
     	super.setText("Error");
     }
